b2715b7b89344b42f88e50387eb17c57fc375a3e
Your Name <you@example.com>
2022-09-11 17:55:36 +0700

    Header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,18 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('content')}>
+                {/* logo */}
+                {/* Search */}
+                {/* login */}
+            </div>
+        </header>
+    );
 }
 
 export default Header;
