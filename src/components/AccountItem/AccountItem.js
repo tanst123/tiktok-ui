6c6007b4035372cfc8d6c863edcd7788551c9339
Your Name <you@example.com>
2022-10-02 18:50:57 +0700

    Giải quyết nhiều file index.js

---
+++ src/components/AccountItem/AccountItem.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem({ data }) {
+    return (
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <img className={cx('avatar')} src={data.avatar} alt={data.full_name} />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('icon-check')} icon={faCheckCircle} />}
+                </p>
+                <p className={cx('username')}>{data.nickname}</p>
+            </div>
+        </Link>
+    );
+}
+
+AccountItem.propTypes = {
+    data: PropTypes.object.isRequired,
+};
+
+export default AccountItem;
--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 
 import styles from './Menu.module.scss';
@@ -6,7 +7,7 @@ import Button from '../../Button';
 const cx = classNames.bind(styles);
 function MenuItem({ data, onClick }) {
     const classes = cx('menu-item', {
-        seperate: data.seperate
+        seperate: data.seperate,
     });
     return (
         <Button className={classes} leftIcon={data.icon} to={data.to} onClick={onClick}>
@@ -15,4 +16,8 @@ function MenuItem({ data, onClick }) {
     );
 }
 
+MenuItem.propTypes = {
+    data: PropTypes.object.isRequired,
+    onClick: PropTypes.func,
+};
 export default MenuItem;
