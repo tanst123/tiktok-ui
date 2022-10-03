3bb5383eea4ce4ac5e05f92fb06268cf9586acd2
Your Name <you@example.com>
2022-10-03 15:45:06 +0700

    Xây dựng UI phần Sidebar #1

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ import Following from '../pages/Following';
 import Profile from '../pages/Profile';
 import Upload from '../pages/Upload';
 import Search from '../pages/Search';
+import Live from '../pages/Live';
 
 // public Route
 const publicRoute = [
@@ -17,6 +18,7 @@ const publicRoute = [
     { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
+    { path: config.routes.live, component: Live },
 ];
 
 const privateRoute = [];
